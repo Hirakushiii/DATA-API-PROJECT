7b12c5c10161026d031c3c2ea55d7add9e2da197
Kenn <141384556+Hirakushiii@users.noreply.github.com>
2024-07-21 14:54:06 +0700

    LAST

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 2007
+const port = 3000
 const bodyParser = require('body-parser')
 const path = require('path');
 const Response = require('./response')
