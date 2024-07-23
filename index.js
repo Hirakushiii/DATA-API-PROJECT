c12bef9d3d99ab8c637b71ddee1fb67ce40d0f18
Kenn <141384556+Hirakushiii@users.noreply.github.com>
2024-07-23 15:14:16 +0700

    LAST

--- index.js
+++ index.js
@@ -6,7 +6,17 @@ const Data = require('./data')
 const Response = require('./response')
 const ApiKey = 'oeBxM7xJCh65BFCftZF6S6AEJ/fAQ9IEVBWIM6ScR2F1h6MbfAKCSv/xRNxeAKt/A7iThuK34WY9s3m0hdm7wQ=='
 app.use(bodyParser.json())
-
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  
+    if (req.method === 'OPTIONS') {
+      res.sendStatus(200);
+    } else {
+      next();
+    }
+});
 
 app.get('/', (req, res) => {
     if (req.query.ApiKey === ApiKey) {
